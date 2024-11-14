378b0959cac55af6e4244aacfa59ae9ad9fb0a0d
looreenz <lorenzo.cremonese10@gmail.com>
2024-11-14 09:47:29 +0100

    Updated HTML and JavaScript files in recepcio/ordesRecepcio directory, including changes to alta/altaOrden.html, alta/altaOrden.js, index.html, and index.js

--- recepcio/ordesRecepcio/alta/altaOrden.js
+++ recepcio/ordesRecepcio/alta/altaOrden.js
@@ -6,281 +6,308 @@ var productID = 0;
 var idorderRception;
 var idOrderLineReception;
 
-window.onload = iniciar;
+window.onload = main;
 
-function iniciar() {
-    cargarProveidor();
-    cargarProductos();
+function main() {
+  cargarProveidor();
+  cargarProductos();
 
-    orderRception = JSON.parse(localStorage.getItem('orderRception'));
-    if (orderRception === null) {
-        orderRception = [];
-    }
+  orderRception = JSON.parse(localStorage.getItem("orderRception"));
+  if (orderRception === null) {
+    orderRception = [];
+  }
 
-    document.getElementById("btnAfegir").addEventListener("click", validar, false);
-    document.getElementById("btnGravar").addEventListener("click", gravarOrden, false);
-    document.getElementById("btnGuardar").addEventListener("click", guardarModificacion, false);
-    document.getElementById("llistatOrden").addEventListener("click", llistarOrden, false);
+  document
+    .getElementById("btnAfegir")
+    .addEventListener("click", validar, false);
+  document
+    .getElementById("btnGravar")
+    .addEventListener("click", gravarOrden, false);
+  document
+    .getElementById("btnGuardar")
+    .addEventListener("click", guardarModificacion, false);
+  document
+    .getElementById("llistatOrden")
+    .addEventListener("click", llistarOrden, false);
 }
 
 function cargarProductos() {
-    const products = JSON.parse(localStorage.getItem("Product")) || [];
-    const productSelect = document.getElementById("product");
+  const products = JSON.parse(localStorage.getItem("Product")) || [];
+  const productSelect = document.getElementById("product");
 
-    products.forEach(product => {
-        const option = document.createElement("option");
-        option.value = product.name; 
-        option.text = product.name;
-        option.setAttribute('id', product.id);
-        productSelect.appendChild(option);
-    });
+  products.forEach((product) => {
+    const option = document.createElement("option");
+    option.value = product.name;
+    option.text = product.name;
+    option.setAttribute("id", product.id);
+    productSelect.appendChild(option);
+  });
 }
 
 function cargarProveidor() {
-    const suppliers = JSON.parse(localStorage.getItem("Supplier")) || [];
-    const supplierSelect = document.getElementById("supplier");
+  const suppliers = JSON.parse(localStorage.getItem("Supplier")) || [];
+  const supplierSelect = document.getElementById("supplier");
 
-    suppliers.forEach(supplier => {
-        const option = document.createElement("option");
-        option.value = supplier.id; 
-        option.text = supplier.name;
-        supplierSelect.appendChild(option);
-    });
+  suppliers.forEach((supplier) => {
+    const option = document.createElement("option");
+    option.value = supplier.id;
+    option.text = supplier.name;
+    supplierSelect.appendChild(option);
+  });
 }
 
 function llistarOrden() {
-    window.location.assign("../llistar/llistatOrden.html");
+  window.location.assign("../llistar/llistatOrden.html");
 }
 
 function afegirProducte() {
-    var producto = document.getElementById("product").value;
-    var cantidadPedida = document.getElementById("quantity_ordered").value;
-    
-    let idObj = ++productID;
-    
-    let productoObj = {
-        id: idObj,
-        product: producto,
-        quantity_ordered: cantidadPedida
-    }
+  var producto = document.getElementById("product").value;
+  var cantidadPedida = document.getElementById("quantity_ordered").value;
 
-    arrTemp.push(productoObj);
+  let idObj = ++productID;
 
-    document.getElementById("product").value = "";
-    document.getElementById("quantity_ordered").value = "";
-    afegirLinea(productoObj);
+  let productoObj = {
+    id: idObj,
+    product: producto,
+    quantity_ordered: cantidadPedida,
+  };
+
+  arrTemp.push(productoObj);
+
+  document.getElementById("product").value = "";
+  document.getElementById("quantity_ordered").value = "";
+  afegirLinea(productoObj);
 }
 
 function afegirLinea(productObj) {
-    var files = document.getElementById('files');
+  var files = document.getElementById("files");
 
-    var linea = document.createElement('tr');
-    linea.setAttribute("id", `fila-${productObj.id}`);
+  var linea = document.createElement("tr");
+  linea.setAttribute("id", `fila-${productObj.id}`);
 
-    var productoTD = document.createElement('td');
-    var textProducto = document.createTextNode(productObj.product);
-    productoTD.appendChild(textProducto);
+  var productoTD = document.createElement("td");
+  var textProducto = document.createTextNode(productObj.product);
+  productoTD.appendChild(textProducto);
 
-    var cantidadOrdenadaTD = document.createElement('td');
-    var textCantidadOrdenada = document.createTextNode(productObj.quantity_ordered);
-    cantidadOrdenadaTD.appendChild(textCantidadOrdenada);
+  var cantidadOrdenadaTD = document.createElement("td");
+  var textCantidadOrdenada = document.createTextNode(
+    productObj.quantity_ordered
+  );
+  cantidadOrdenadaTD.appendChild(textCantidadOrdenada);
 
-    var esborrarTD = document.createElement('td');
-    var checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.value = 'opcio';
-    checkbox.addEventListener("click", () => borrarLineReception(productObj.id));
-    esborrarTD.appendChild(checkbox);
+  var esborrarTD = document.createElement("td");
+  var checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.value = "opcio";
+  checkbox.addEventListener("click", () => borrarLineReception(productObj.id));
+  esborrarTD.appendChild(checkbox);
 
-    var modificarTD = document.createElement('td');
-    var buttonModificar = document.createElement('button');
-    buttonModificar.textContent = 'Modificar';
-    buttonModificar.className = "btn btn-primary btn-lg";
-    buttonModificar.addEventListener("click", () => modificarLineReception(productObj.id, productObj.product, productObj.quantity_ordered));
-    modificarTD.appendChild(buttonModificar);
+  var modificarTD = document.createElement("td");
+  var buttonModificar = document.createElement("button");
+  buttonModificar.textContent = "Modificar";
+  buttonModificar.className = "btn btn-primary btn-lg";
+  buttonModificar.addEventListener("click", () =>
+    modificarLineReception(
+      productObj.id,
+      productObj.product,
+      productObj.quantity_ordered
+    )
+  );
+  modificarTD.appendChild(buttonModificar);
 
-    linea.appendChild(productoTD);
-    linea.appendChild(cantidadOrdenadaTD);
-    linea.appendChild(esborrarTD);productID
-    linea.appendChild(modificarTD);
+  linea.appendChild(productoTD);
+  linea.appendChild(cantidadOrdenadaTD);
+  linea.appendChild(esborrarTD);
+  productID;
+  linea.appendChild(modificarTD);
 
-    files.appendChild(linea);
+  files.appendChild(linea);
 }
 
 function borrarLineReception(productId) {
-    arrTemp = arrTemp.filter(producto => producto.id !== productId);
+  arrTemp = arrTemp.filter((producto) => producto.id !== productId);
 
-    var fila = document.getElementById(`fila-${productId}`);
-    if (fila) {
-        fila.remove();
-    }
+  var fila = document.getElementById(`fila-${productId}`);
+  if (fila) {
+    fila.remove();
+  }
 }
 
 var productoEditadoID = null;
 function modificarLineReception(productID, product, quantity_ordered) {
-    productoEditadoID = productID;
+  productoEditadoID = productID;
 
-    document.getElementById("btnGuardar").style.display = "inline-block";
-    document.getElementById("btnAfegir").style.display = "none";
-    document.getElementById("btnGravar").style.display = "none";
-    document.getElementById('llistatOrden').style.display = "none";
+  document.getElementById("btnGuardar").style.display = "inline-block";
+  document.getElementById("btnAfegir").style.display = "none";
+  document.getElementById("btnGravar").style.display = "none";
+  document.getElementById("llistatOrden").style.display = "none";
 
-    document.getElementById("product").value = product;
-    document.getElementById("quantity_ordered").value = quantity_ordered;
+  document.getElementById("product").value = product;
+  document.getElementById("quantity_ordered").value = quantity_ordered;
 }
 
 function guardarModificacion() {
-    const nuevoProducto = document.getElementById("product").value;
-    const nuevaCantidad = document.getElementById("quantity_ordered").value;
+  const nuevoProducto = document.getElementById("product").value;
+  const nuevaCantidad = document.getElementById("quantity_ordered").value;
 
-    const productObj = arrTemp.find(product => product.id === productoEditadoID);
-    if (productObj) {
-        productObj.product = nuevoProducto;
-        productObj.quantity_ordered = nuevaCantidad;
-    }
+  const productObj = arrTemp.find(
+    (product) => product.id === productoEditadoID
+  );
+  if (productObj) {
+    productObj.product = nuevoProducto;
+    productObj.quantity_ordered = nuevaCantidad;
+  }
 
-    const fila = document.getElementById(`fila-${productoEditadoID}`);
+  const fila = document.getElementById(`fila-${productoEditadoID}`);
 
-    if (fila && fila.children.length > 1) {
-        fila.children[0].textContent = nuevoProducto;
-        fila.children[1].textContent = nuevaCantidad;
-    } 
+  if (fila && fila.children.length > 1) {
+    fila.children[0].textContent = nuevoProducto;
+    fila.children[1].textContent = nuevaCantidad;
+  }
 
-    document.getElementById("product").value = "";
-    document.getElementById("quantity_ordered").value = "";
+  document.getElementById("product").value = "";
+  document.getElementById("quantity_ordered").value = "";
 
-    document.getElementById("btnGuardar").style.display = "none";
-    document.getElementById("btnGravar").style.display = "block";
-    document.getElementById('llistatOrden').style.display = "inline-block";
-    document.getElementById("btnAfegir").style.display = "inline-block";
+  document.getElementById("btnGuardar").style.display = "none";
+  document.getElementById("btnGravar").style.display = "block";
+  document.getElementById("llistatOrden").style.display = "inline-block";
+  document.getElementById("btnAfegir").style.display = "inline-block";
 
-    productoEditadoID = null;
+  productoEditadoID = null;
 }
 
 function gravarOrden() {
-    let idorderRception;
-    let idOrderLineReception;
+  let idorderRception;
+  let idOrderLineReception;
 
-    let orderReception = JSON.parse(localStorage.getItem('orderRception')) || [];
-    let orderLineReception = JSON.parse(localStorage.getItem('orderLineReception')) || [];
+  let orderReception = JSON.parse(localStorage.getItem("orderRception")) || [];
+  let orderLineReception =
+    JSON.parse(localStorage.getItem("orderLineReception")) || [];
 
-    if(orderReception.length == 0) {
-        idorderRception = 1;
-    } else {
-        const maxObj = orderRception.reduce((max, obj) => (obj.id > max.id ? obj : max), orderReception[0]);
-        idorderRception = ++ maxObj.id;
-    }
-    
-    var supplier = document.getElementById('supplier').value;
-    var dataEstimada = document.getElementById("estimated_reception_date").value;
-    
-    let order = {
-        id: idorderRception,
-        supplier: supplier,
-        estimated_reception_date: dataEstimada,
-        created_by: 1,
-        orderreception_status_id: 1
-    }
+  if (orderReception.length == 0) {
+    idorderRception = 1;
+  } else {
+    const maxObj = orderRception.reduce(
+      (max, obj) => (obj.id > max.id ? obj : max),
+      orderReception[0]
+    );
+    idorderRception = ++maxObj.id;
+  }
 
-    orderRception.push(order);
+  var supplier = document.getElementById("supplier").value;
+  var dataEstimada = document.getElementById("estimated_reception_date").value;
 
-    localStorage.setItem('orderRception', JSON.stringify(orderRception));
-   
+  let order = {
+    id: idorderRception,
+    supplier: supplier,
+    estimated_reception_date: dataEstimada,
+    created_by: 1,
+    orderreception_status_id: 1,
+  };
 
+  orderRception.push(order);
 
-    if(orderLineReception.length == 0) {
-        idOrderLineReception = 0;
-    } else {
-        const maxObj = orderLineReception.reduce((max, obj) => (obj.id > max.id ? obj : max), orderLineReception[0]);
-        idOrderLineReception = maxObj.id;
-    }
-    
-    arrTemp.forEach(product => {
-        product.id = ++idOrderLineReception;
-        product.order_reception_id = idorderRception;
-        orderLineReception.push(product);
-    });
+  localStorage.setItem("orderRception", JSON.stringify(orderRception));
 
-    localStorage.setItem('orderLineReception', JSON.stringify(orderLineReception));
+  if (orderLineReception.length == 0) {
+    idOrderLineReception = 0;
+  } else {
+    const maxObj = orderLineReception.reduce(
+      (max, obj) => (obj.id > max.id ? obj : max),
+      orderLineReception[0]
+    );
+    idOrderLineReception = maxObj.id;
+  }
 
-    alert('Guardad0 correctamente');
+  arrTemp.forEach((product) => {
+    product.id = ++idOrderLineReception;
+    product.order_reception_id = idorderRception;
+    orderLineReception.push(product);
+  });
+
+  localStorage.setItem(
+    "orderLineReception",
+    JSON.stringify(orderLineReception)
+  );
+
+  alert("Guardad0 correctamente");
 }
 
-
 function validarProveidor() {
-    var proveidor = document.getElementById("supplier");
-    if (!proveidor.checkValidity()) {
-        if (proveidor.validity.valueMissing) {
-            error(proveidor, "Selecciona un proveedor");
-        }
-        return false;
+  var proveidor = document.getElementById("supplier");
+  if (!proveidor.checkValidity()) {
+    if (proveidor.validity.valueMissing) {
+      error(proveidor, "Selecciona un proveedor");
     }
-    return true;
+    return false;
+  }
+  return true;
 }
 
 function validarDataEstima() {
-    var dataEstimada = document.getElementById("estimated_reception_date");
-    if (!dataEstimada.checkValidity()) {
-        if (dataEstimada.validity.valueMissing) {
-            error(dataEstimada, "Selecciona una data esperada de recepción");
-        }
-        return false;
+  var dataEstimada = document.getElementById("estimated_reception_date");
+  if (!dataEstimada.checkValidity()) {
+    if (dataEstimada.validity.valueMissing) {
+      error(dataEstimada, "Selecciona una data esperada de recepción");
     }
-    return true;
+    return false;
+  }
+  return true;
 }
-
 
 function validarProducto() {
-    var proveidor = document.getElementById("product");
-    if (!proveidor.checkValidity()) {
-        if (proveidor.validity.valueMissing) {
-            error(proveidor, "Selecciona un producto");
-        }
-        return false;
+  var proveidor = document.getElementById("product");
+  if (!proveidor.checkValidity()) {
+    if (proveidor.validity.valueMissing) {
+      error(proveidor, "Selecciona un producto");
     }
-    return true;
+    return false;
+  }
+  return true;
 }
 
-
 function validarCantidadPedida() {
-    var cantidadPedida = document.getElementById("quantity_ordered");
-    if (!cantidadPedida.checkValidity()) {
-        if (cantidadPedida.validity.valueMissing) {
-            error(cantidadPedida, "Introduce la cantidad pedida");
-        }
-        else if (cantidadPedida.validity.patternMismatch) {
-            error(cantidadPedida, "Introduce solo numeros");
-        }
-        return false;
+  var cantidadPedida = document.getElementById("quantity_ordered");
+  if (!cantidadPedida.checkValidity()) {
+    if (cantidadPedida.validity.valueMissing) {
+      error(cantidadPedida, "Introduce la cantidad pedida");
+    } else if (cantidadPedida.validity.patternMismatch) {
+      error(cantidadPedida, "Introduce solo numeros");
     }
-    return true;
+    return false;
+  }
+  return true;
 }
 
 function validar(e) {
-    esborrarError();
-    e.preventDefault();
+  esborrarError();
+  e.preventDefault();
 
-    if (validarProveidor() && validarDataEstima() && validarProducto() && validarCantidadPedida()) {
-        afegirProducte();
-        return true;
-
-    } else {
-        return false;
-    }
+  if (
+    validarProveidor() &&
+    validarDataEstima() &&
+    validarProducto() &&
+    validarCantidadPedida()
+  ) {
+    afegirProducte();
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function error(element, missatge) {
-    const textError = document.createTextNode(missatge);
-    const elementError = document.getElementById("missatgeError")
-    elementError.appendChild(textError)
-    element.classList.add("error")
-    element.focus();
+  const textError = document.createTextNode(missatge);
+  const elementError = document.getElementById("missatgeError");
+  elementError.appendChild(textError);
+  element.classList.add("error");
+  element.focus();
 }
 
 function esborrarError() {
-    let formulari = document.forms[0].elements;
-    for (let ele of formulari) {
-        ele.classList.remove("error")
-    }
-    document.getElementById("missatgeError").replaceChildren();
+  let formulari = document.forms[0].elements;
+  for (let ele of formulari) {
+    ele.classList.remove("error");
+  }
+  document.getElementById("missatgeError").replaceChildren();
 }
