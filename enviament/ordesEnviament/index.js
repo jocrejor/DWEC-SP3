b6b7b499d12adf8e4673b93c5a52be396372c4cd
JavierCornejoLeal <cornejolealjavier@gmail.com>
2024-11-19 09:22:31 +0100

    Eliminados todos los archivos de la rama Javier

--- enviament/ordesEnviament/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    document.getElementById("carregar").addEventListener("click", carregar)
-});
-
-function carregar () {
-    localStorage.setItem("clients", JSON.stringify(Client));
-    localStorage.setItem("products", JSON.stringify(Product));
-    localStorage.setItem("carriers", JSON.stringify(Carriers));
-}
