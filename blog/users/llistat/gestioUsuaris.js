5dd6496f9eb4f45ffd68c94bade1d44874142fe4
Andrea Mendez <andbermen@alu.edu.gva.es>
2024-11-26 00:09:22 +0100

    Blog - Gestion de usuarios/listar funcional

--- blog/users/llistat/gestioUsuaris.js
+++ blog/users/llistat/gestioUsuaris.js
@@ -1,5 +1,7 @@
 window.onload = iniciar;
 
+url = 'http://localhost:5002/';
+
 function iniciar() {
     document.getElementById("nouUsuari").addEventListener("click", altaUsuari);
     document.getElementById("backToBlog").addEventListener("click", backToBlog);
@@ -10,9 +12,10 @@ function gestioUsuaris() {
     defineRoles();
 }
 
-function listUsers() {
-    var users = JSON.parse(localStorage.getItem("users")) || []; // Obtener los usuarios
+async function listUsers() {
+    var users = await getData(url, 'Users') ?? []; // Obtener los usuarios
     var tbody = document.getElementById("files");
+    console.log(users);
 
     // Limpiar el contenido actual del tbody
     while (tbody.firstChild) {
@@ -71,11 +74,13 @@ function listUsers() {
 }
 
 function altaUsuari() {
-    window.location.href = "altaUsuaris.html";
+    window.location.href = "../alta/altaUsuaris.html";
 }
 
-function deleteUser(id) {
-    var users = JSON.parse(localStorage.getItem("users")) || []; // Obtener los usuarios
+async function deleteUser(id) {
+    deleteData(url, 'Users', id);
+
+    /*var users = JSON.parse(localStorage.getItem("users")) || []; // Obtener los usuarios
 
     for (var i = 0; i < users.length; i++) {
         var user = users[i];
@@ -84,24 +89,24 @@ function deleteUser(id) {
             break;
         }
     }
-    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("users", JSON.stringify(users));*/
     listUsers();
 }
 
-function modifyUser(id) {
-    var users = JSON.parse(localStorage.getItem("users")) || []; // Obtener los usuarios
-
+async function modifyUser(id) {
+    //var users = JSON.parse(localStorage.getItem("users")) || []; // Obtener los usuarios
+    let users = await getData(url, 'Users');
     for (var i = 0; i < users.length; i++) {
         var user = users[i];
         if (user.id === id) {
             // Guardar los datos del usuario en localStorage temporalmente
             localStorage.setItem("userToEdit", JSON.stringify(users[i]));
-            window.location.href = "modificarUsuari.html";
+            window.location.href = "../modificar/modificarUsuari.html";
             break;
         }
     }
 }
 
 function backToBlog() {
-    window.location.href = "blog.html";
+    window.location.href = "../../index.html";
 }
