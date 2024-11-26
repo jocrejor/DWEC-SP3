b68156a07386436c5fba0940631f72b342c378d3
carmol02 <121013225+carmol02@users.noreply.github.com>
2024-11-26 23:12:30 +0100

    Add files via upload

--- magatzem/gestioMagatzem/Espais/modificar/modificarSpace.js
+++ magatzem/gestioMagatzem/Espais/modificar/modificarSpace.js
@@ -6,8 +6,13 @@ function iniciar() {
     if (modSpace) {
         document.getElementById("id").value = modSpace.id;
         document.getElementById("nom").value = modSpace.nom;
+        document.getElementById("producte").value = modSpace.producte;
+        document.getElementById("quantitat").value = modSpace.quantitat;
         document.getElementById("volum").value = modSpace.volum;
         document.getElementById("pes").value = modSpace.pes;
+        document.getElementById("magatzem").value = modSpace.magatzem;
+        document.getElementById("carrer").value = modSpace.carrer;
+        document.getElementById("estanteria").value = modSpace.estanteria;
     }
 
     document.getElementById("btnModificar").addEventListener("click", modificar);
@@ -17,21 +22,32 @@ function iniciar() {
 function modificar() {
     const id = document.getElementById("id").value;
     const nom = document.getElementById("nom").value;
+    const producte = document.getElementById("producte").value;
+    const quantitat = document.getElementById("quantitat").value;
     const volum = document.getElementById("volum").value;
     const pes = document.getElementById("pes").value;
+    const magatzem = document.getElementById("magatzem").value;
+    const carrer = document.getElementById("carrer").value;
+    const estanteria = document.getElementById("estanteria").value;
 
     let arrSpaces = JSON.parse(localStorage.getItem("spaces")) || [];
 
     for (let i = 0; i < arrSpaces.length; i++) {
         if (arrSpaces[i].id == id) {
             arrSpaces[i].nom = nom;
+            arrSpaces[i].producte = producte;
+            arrSpaces[i].quantitat = quantitat;
             arrSpaces[i].volum = volum;
             arrSpaces[i].pes = pes;
+            arrSpaces[i].magatzem = magatzem;
+            arrSpaces[i].carrer = carrer;
+            arrSpaces[i].estanteria = estanteria;
             break;
         }
     }
 
     localStorage.setItem("spaces", JSON.stringify(arrSpaces));
+    alert("Modificament fet");
     window.location.assign("../llista/llistatSpace.html");
 }
 
