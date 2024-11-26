a5b6b6cf2111526008fa114ce7e353abf10f4cdf
lorsis <119575615+lorsis@users.noreply.github.com>
2024-11-26 22:08:35 +0100

    Update LlistaMagatzem.js
    canvis

--- magatzem/gestioMagatzem/Magatzem/Llistar/LlistaMagatzem.js
+++ magatzem/gestioMagatzem/Magatzem/Llistar/LlistaMagatzem.js
@@ -1,15 +1,16 @@
 window.onload = main;
-let storages
+let storages;
+
 function main() {
     document.getElementById("producte").addEventListener("click", nou);
     carregarInformacio();
 }
 
 async function carregarInformacio() {
-     storages = await getData(url,"Storage")
-     obtindreMagatzem();
-    
+    storages = await getData(url, "Storage");
+    obtindreMagatzem();
 }
+
 function nou() {
     window.location.assign("../Nou/nouMagatzem.html");
 }
@@ -20,33 +21,33 @@ function obtindreMagatzem() {
 
     storages.forEach((storage) => {
         let row = `
-            <tr id="${storage.id}">
-                <td><button class="btn btn-danger" onclick="esborrar(${storage.id})">Esborrar</button></td>
-                <td><button class="btn btn-primary" onclick="modificar(${storage})">Modificar</button></td>
-                <td><button class="btn btn-primary" onclick="carrers(${storage.id})">Carrer</button></td>
-
-                <td>${storage.id || ""}</td>
-                <td>${storage.name || ""}</td>
-                <td>${storage.type || ""}</td>
-                <td>${storage.address || ""}</td>
-            </tr>
-        `;
+        <tr id="${storage.id}">
+            <td><button class="btn btn-danger" onclick="esborrar(${storage.id})">Esborrar</button></td>
+            <td><button class="btn btn-primary" onclick="modificar(${storage.id})">Modificar</button></td>
+            <td><button class="btn btn-primary" onclick="carrers(${storage.id})">Carrer</button></td>
+   
+            <td>${storage.id || ""}</td>
+            <td>${storage.name || ""}</td>
+            <td>${storage.type || ""}</td>
+            <td>${storage.address || ""}</td>
+        </tr>
+   `;
         tbody.innerHTML += row;
     });
 }
 
 async function esborrar(id) {
-    await deleteData(url, "Storage", id)
-    document.getElementById("id").remove();
-
+   
+    await deleteData(url, "Storage", id);
+    document.getElementById(id).remove();
 }
 
-function modificar(storage) {
-    localStorage.setItem("modificaMagatzem", JSON.stringify(storage));
-    window.location.assign("../Modificar/modificarMagatzem.html");
+function modificar(storageId) {
+ 
+        localStorage.setItem("modificaMagatzem", JSON.stringify(storageId));
+        window.location.assign("../Modificar/modificarMagatzem.html");
 }
 
 function carrers(storageId) {
     window.location.assign(`../../Carrer/Llistar/LlistatCarrer.html`);
 }
-
