015f205212a5a3e4b20b1328de60e0f84ba724dc
jocrejor <jocrejor@gmail.com>
2024-11-28 08:52:04 +0100

    Ajuda a gabi de david

--- magatzem/moviments/moviments.js
+++ magatzem/moviments/moviments.js
@@ -1,10 +1,7 @@
 // URL base i endpoint del servidor
-const urlBase = "http://localhost:5001/";
-const endPointStorage = "Storage";
-const endPointStreet = "Street";
-const endPointShelf = "Shelf";
-const endPointSpace ="Space";
-const endPointProduct ="Product";
+let arrayMoviments =[];
+let arrayProduct = [];
+let arrayUser =[];
 
 window.onload = main;
 
@@ -12,111 +9,29 @@ window.onload = main;
  * Funció principal que s'executa en carregar la pàgina.
  * Configura els events dels botons i crida a les funcions per a crear la taula i llistar els estats.
  */
-function main() {
-    getEstatsStorage();
-    getEstatsStreet();
-    getEstatsShelf();
-    getEstatsSpace();
-    getEstatsProduct();
+async function main() {
+    arrayMoviments = await getData (url,"Moviment"); 
+    arrayProduct = await getData (url,"Product");
+    arrayUser = await getData (url,"User");
+    mostrarTaula();
 }
 
-/**
- * Obté els estats des de la base de dades de  i els mostra a la taula.
- */
-async function getEstatsStorage() {
-    try {
-        const data = await getData(urlBase,endPointStorage);
-
-        // Comprovació
-        if (data && Array.isArray(data)) {
-            mostrarTaula(data);
-        } else{
-            console.error('Error al obtenir els estats del servidor');
-        }
-
-    } catch (error) {
-        console.error('Error al carregar els estats:', error);
-    }
+function getProductName (id){
+    const obj = arrayProduct.find(prod => id == prod.id);
+    return obj.name;
 }
 
-async function getEstatsStreet() {
-    try {
-        const data = await getData(urlBase,endPointStreet);
 
-        // Comprovació
-        if (data && Array.isArray(data)) {
-            mostrarTaula(data);
-        } else{
-            console.error('Error al obtenir els estats del servidor');
-        }
-
-    } catch (error) {
-        console.error('Error al carregar els estats:', error);
-    }
-}
-
-async function getEstatsShelf() {
-    try {
-        const data = await getData(urlBase,endPointShelf);
-
-        // Comprovació
-        if (data && Array.isArray(data)) {
-            mostrarTaula(data);
-        } else{
-            console.error('Error al obtenir els estats del servidor');
-        }
-
-    } catch (error) {
-        console.error('Error al carregar els estats:', error);
-    }
-}
-
-async function getEstatsSpace() {
-    try {
-        const data = await getData(urlBase,endPointSpace);
-
-        // Comprovació
-        if (data && Array.isArray(data)) {
-            mostrarTaula(data);
-        } else{
-            console.error('Error al obtenir els estats del servidor');
-        }
-
-    } catch (error) {
-        console.error('Error al carregar els estats:', error);
-    }
-}
-
-async function getEstatsProduct() {
-    try {
-        const data = await getData(urlBase,endPointProduct);
-
-        // Comprovació
-        if (data && Array.isArray(data)) {
-            mostrarTaula(data);
-        } else{
-            console.error('Error al obtenir els estats del servidor');
-        }
-
-    } catch (error) {
-        console.error('Error al carregar els estats:', error);
-    }
-}
-
-/**
- * Mostra els estats a la taula HTML.
- * @param {Array} estats - Llista d'estats obtinguts des de la base de dades.
- */
-function mostrarTaula(estats) {
+function mostrarTaula() {
     const tablaContenido = document.getElementById("tablaContenido");
     tablaContenido.innerHTML = "";
     
-    estats.forEach(estat => {
+    arrayMoviments.forEach(mov => {
         const fila = document.createElement("tr");
         
         fila.innerHTML = `
-            <td>1</td>
-            <td>2</td>
+            <td>${getProductName( mov.product_id)}</td>
+            <td>${mov.storage_id}</td>
             <td>3</td>
             <td>4</td>
             <td>5</td>
