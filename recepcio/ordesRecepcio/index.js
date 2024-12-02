6bd23ba560ef64d174bbda1ae77ab37fad233969
lorenzo <lorenzo.cremonese10@gmail.com>
2024-12-02 19:46:31 +0100

    "Deleted index.html and index.js files from recepcio/ordesRecepcio directory."

--- recepcio/ordesRecepcio/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  document.getElementById("ordens").addEventListener("click", llistar);
-});
-
-function llistar() {
-  location.assign("./llistar/llistatOrden.html");
-}
