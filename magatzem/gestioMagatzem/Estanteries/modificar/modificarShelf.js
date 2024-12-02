780f2ca0987fd00cef60dc35d3df84fef6080e6d
carmol02 <121013225+carmol02@users.noreply.github.com>
2024-12-02 12:47:42 +0100

    Update modificarShelf.js

--- magatzem/gestioMagatzem/Estanteries/modificar/modificarShelf.js
+++ magatzem/gestioMagatzem/Estanteries/modificar/modificarShelf.js
@@ -1,28 +1,31 @@
 window.onload = iniciar;
 
 function iniciar() {
-    const shelf = JSON.parse(localStorage.getItem("modShelf")) || {};
+    const shelf = JSON.parse(localStorage.getItem("Estanteria"));
     carregarDades(shelf);
 
     document.getElementById("btnGuardar").addEventListener("click", guardarModificacions, false);
-    document.getElementById("btnCancelar").addEventListener("click", () => window.location.assign("../llista/llistatShelf.html"));
+    document.getElementById("btnCancelar").addEventListener("click", () =>   window.location.assign("../llista/llistatShelf.html"));
 }
 
-function carregarDades(shelf) {
-    document.getElementById("id").value = shelf.id;
-    document.getElementById("nom").value = shelf.nom;
-    document.getElementById("id_carrer").value = shelf.id_carrer;
-    document.getElementById("id_magatzem").value = shelf.id_magatzem;
-    
+async function carregarDades(shelf) {
+    const estanteria = await getData(url,"Shelf");
+    const estanteriaTriada = estanteria.filter(o => o.id === shelf);
+    document.getElementById("id").value = estanteriaTriada.id;
+    alert("hola");
+    document.getElementById("name").value = estanteriaTriada.name;
+    document.getElementById("storage_id").value = estanteriaTriada.storage_id;
+    document.getElementById("steet_id").value = estanteriaTriada.steet_id;
+     
 }
 
 function guardarModificacions(e) {
     e.preventDefault();
 
     const id = document.getElementById("id").value;
-    const nom = document.getElementById("nom").value;
-    const id_magatzem = document.getElementById("id_magatzem").value;
-    const id_carrer = document.getElementById("id_carrer").value;
+    const nom = document.getElementById("name").value;
+    const id_magatzem = document.getElementById("sotrgae_id").value;
+    const id_carrer = document.getElementById("steet_Id").value;
 
     const estanteries = JSON.parse(localStorage.getItem("shelfs")) || [];
     const index = estanteries.findIndex((e) => e.id === id);
